fc0050692b097b913405a87a8baa02f2259e3286
kalunge <muthomititus+github@gmail.com>
2022-06-15 18:49:44 +0300

    Move dodger to right and left

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+const moveDodgerLeft = () => {
+  let dodger = document.querySelector('#dodger');
+  dodger.style.left = 0;
+};
+
+// style="bottom: 0px; left: 180px;"
+
+
+const moveDodgerRight = () => {
+  let dodger = document.querySelector('#dodger');
+  dodger.style.left = "180px";
+  dodger.style.right = 0;
+}
